907b26216e51bcea8674c21c436fe6791851b5c1
Fyodor <feodorinin@gmail.com>
2023-01-19 15:36:56 +0300

    Добавил решение для первой задачи

--- 1task/Program.cs
+++ 1task/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/* Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16 */
+
+Console.Clear();
+
+void Pow(int p, int q)
+{
+    int result = p, i = 1;
+    while (i < q)
+    {
+        result = result * p;
+        i++;
+    }
+    Console.WriteLine($"A в степени В равно {result}");
+}
+
+int A = int.Parse(Console.ReadLine());
+int B = int.Parse(Console.ReadLine());
+Pow(A, B);
