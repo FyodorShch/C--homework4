aee5728e0402cff16d2eca5b8da522e5ee638de1
Fyodor <feodorinin@gmail.com>
2023-01-19 17:08:39 +0300

    Добавил .gitignore и решение для 3й задачи

---
+++ 3task/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33] */
+
+Console.Clear();
+
+void arr(int N, int start, int end)
+{
+    int[] RandArray = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        RandArray[i] = new Random().Next(start, end + 1);
+    }
+    Console.Write("[");
+    for(int j=0;j<N;j++)
+    {
+        Console.Write($" {RandArray[j]}, ");
+    }
+    Console.Write("]");
+}
+
+int A = 8;
+arr(A, 0, 100);
